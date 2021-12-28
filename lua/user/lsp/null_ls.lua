c4e49a7c889a2b4702a0666be154368291ddbf43
shendy pratama <shendypratamaa@gmail.com>
2021-12-29 04:59:58 +0700

    fix npm global configuration on local env for running null lsp linting and formating documents, and source code

--- lua/user/lsp/null_ls.lua
+++ lua/user/lsp/null_ls.lua
@@ -1,35 +1,40 @@
-local status_ok, null_ls = pcall(require, 'null-ls')
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 
-if not status_ok then
-  return
+if not null_ls_status_ok then
+	return
 end
 
 local formatting = null_ls.builtins.formatting
 
 local actions = null_ls.builtins.code_actions
 
+local sources = {
+	formatting.eslint_d,
+	formatting.stylua,
+	formatting.stylelint,
+	formatting.prettier.with({
+		filetype = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+			"vue",
+			"html",
+			"json",
+			"yaml",
+			"markdown",
+		},
+		extra_args = { "--no-semi", "--single-quotes", "--jsx-single-quotes", "--tsx-single-quotes" },
+	}),
+	formatting.black.with({ extra_args = { "--fast" } }),
+	actions.gitsigns,
+}
+
 null_ls.setup({
-  debug = false,
-  sources = {
-    formatting.prettier.with({
-      filetype = {
-        'vim',
-        'lua',
-        'javascript',
-        'javascriptreact',
-        'typescriptreact',
-        'typescript',
-        'html',
-        'markdown',
-        'css',
-        'scss',
-        'json',
-        'yaml'
-      },
-      extra_args = {"--no-semi", "--jsx-single-quotes", "--tsx-single-quotes", "--single-quotes"}
-    }),
-    formatting.black.with({ extra_args = { "--fast" } }),
-    formatting.ESLint,
-    actions.gitsigns
-  }
+	sources = sources,
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		end
+	end,
 })
