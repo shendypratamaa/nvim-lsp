6450fe5944479e1a2b1548352b173e18eeae72dc
shendy pratama <shendypratamaa@gmail.com>
2022-01-02 20:08:00 +0700

    fix spacing with prettier, for specified files bufferline.lua cmp.lua colorizer.lua indentblankline.lua nvimtree.lua telescope.lua trouble.lua

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -1,66 +1,65 @@
-local status_ok, buff = pcall(require, 'bufferline')
+local status_ok, buff = pcall(require, "bufferline")
 
 if not status_ok then
-  return
+	return
 end
 
-buff.setup {
-  options = {
-    numbers = function(opts)
-     return string.format('%s', opts.lower(opts.ordinal))
-    end,
-    close_command = "bdelete! %d",
-    right_mouse_command = "bdelete! %d",
-    left_mouse_command = "buffer %d",
-    middle_mouse_command = nil,
-    indicator_icon = '',
-    buffer_close_icon = '',
-    modified_icon = '●',
-    close_icon = '',
-    left_trunc_marker = '',
-    right_trunc_marker = '',
-    name_formatter = function(buf)
-      if buf.name:match('%.md') then
-        return vim.fn.fnamemodify(buf.name, ':t:r')
-      end
-    end,
-   offsets = {
-      {
-      filetype = "tree",
-      text = function()
-        return vim.fn.getcwd()
-      end,
-      highlight = "Directory",
-      text_align = "center"
-      }
-    },
-    max_name_length = 18,
-    max_prefix_length = 15,
-    tab_size = 20,
-    diagnostics = "diagnostics",
-    diagnostics_update_in_insert = true,
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      return "("..count..")"
-    end,
-    custom_filter = function(buf_number)
-      if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-        return true
-      end
-      if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-        return true
-      end
-      if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-        return true
-      end
-    end,
-    show_buffer_icons = true,
-    show_buffer_close_icons = true,
-    show_close_icon = true,
-    show_tab_indicators = false,
-    persist_buffer_sort = false,
-    separator_style = { '  ', '  ' },
-    enforce_regular_tabs = true,
-    always_show_bufferline = true,
-    sort_by = "id",
-  }
-}
+buff.setup({
+	options = {
+		numbers = nil,
+		close_command = "bdelete! %d",
+		right_mouse_command = "bdelete! %d",
+		left_mouse_command = "buffer %d",
+		middle_mouse_command = nil,
+		indicator_icon = "🚀",
+		buffer_close_icon = "",
+		modified_icon = "●",
+		close_icon = "",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
+		name_formatter = function(buf)
+			if buf.name:match("%.md") then
+				return vim.fn.fnamemodify(buf.name, ":t:r")
+			end
+		end,
+		offsets = {
+			{
+				filetype = "NvimTree",
+				text = function()
+					return vim.fn.getcwd()
+				end,
+				highlight = "Directory",
+				text_align = "center",
+				padding = 1,
+			},
+		},
+		max_name_length = 18,
+		max_prefix_length = 15,
+		tab_size = 21,
+		diagnostics = false,
+		diagnostics_update_in_insert = true,
+		-- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+		-- 	return "(" .. count .. ")"
+		-- end,
+		-- custom_filter = fonction(buf_number)
+		-- 	if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+		-- 		return true
+		-- 	end
+		-- 	if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+		-- 		return true
+		-- 	end
+		-- 	if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+		-- 		return true
+		-- 	end
+		-- end,
+		show_buffer_icons = true,
+		show_buffer_close_icons = true,
+		show_close_icon = true,
+		show_tab_indicators = true,
+		persist_buffer_sort = true,
+		separator_style = { "  ", "  " },
+		enforce_regular_tabs = true,
+		always_show_bufferline = true,
+		sort_by = "id",
+	},
+})
--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,115 +1,124 @@
-local status_ok, telescope = pcall(require, 'telescope')
+local status_ok, telescope = pcall(require, "telescope")
 
-if not status_ok then 
-  return
+local trouble_status, trouble = pcall(require, "trouble.provider.telescope")
+
+if not trouble_status then
+	return
 end
 
-local actions = require('telescope.actions')
+if not status_ok then
+	return
+end
 
-telescope.setup{
-  extension = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-      case_mode = 'smart-case'
-    }
-  },
-  defaults = {
-    prompt_prefix = " ",
-    selection_caret = " ",
-    path_display = { "smart" },
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '--trim'
-    },
-    mappings = {
-      i = {
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
+local actions = require("telescope.actions")
 
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
+telescope.setup({
+	extension = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart-case",
+		},
+	},
+	defaults = {
+		prompt_prefix = " ",
+		selection_caret = " ",
+		path_display = { "smart" },
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"--trim",
+			"--hidden",
+		},
+		mappings = {
+			i = {
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
 
-        ["<C-c>"] = actions.close,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
 
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
+				["<C-c>"] = actions.close,
 
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
 
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
 
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
 
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-        ["<C-l>"] = actions.complete_tag,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-      },
-      n = {
-        ["<esc>"] = actions.close,
-        ["<CR>"] = actions.select_default,
-        ["<C-x>"] = actions.select_horizontal,
-        ["<C-v>"] = actions.select_vertical,
-        ["<C-t>"] = actions.select_tab,
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
 
-        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-l>"] = actions.complete_tag,
+				["<C-_>"] = actions.which_key,
+				["<C-j"] = trouble.open_with_trouble,
+			},
+			n = {
+				["<esc>"] = actions.close,
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+				["<C-c>"] = actions.close,
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 
-        ["j"] = actions.move_selection_next,
-        ["k"] = actions.move_selection_previous,
-        ["H"] = actions.move_to_top,
-        ["M"] = actions.move_to_middle,
-        ["L"] = actions.move_to_bottom,
+				["j"] = actions.move_selection_next,
+				["k"] = actions.move_selection_previous,
+				["H"] = actions.move_to_top,
+				["M"] = actions.move_to_middle,
+				["L"] = actions.move_to_bottom,
 
-        ["<Down>"] = actions.move_selection_next,
-        ["<Up>"] = actions.move_selection_previous,
-        ["gg"] = actions.move_to_top,
-        ["G"] = actions.move_to_bottom,
+				["<Down>"] = actions.move_selection_next,
+				["<Up>"] = actions.move_selection_previous,
+				["gg"] = actions.move_to_top,
+				["G"] = actions.move_to_bottom,
 
-        ["<C-u>"] = actions.preview_scrolling_up,
-        ["<C-d>"] = actions.preview_scrolling_down,
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
 
-        ["<PageUp>"] = actions.results_scrolling_up,
-        ["<PageDown>"] = actions.results_scrolling_down,
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
+				["<c-j>"] = trouble.open_with_trouble,
 
-        ["?"] = actions.which_key,
-      },
-    },
-    entry_prefix = "",
-    initial_mode = "insert",
-    selection_strategy = "reset",
-    sorting_strategy = "descending",
-    layout_strategy = "horizontal",
-    file_ignore_patterns = {},
-    border = {},
-    winblend = 0,
-    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-    color_devicons = true,
-    use_less = true,
-    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
-    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-    generic_sorter =  require'telescope.sorters'.get_fzf_sorter,
-    extension = require'telescope'.load_extension('fzf')
-  },
-}
+				["?"] = actions.which_key,
+			},
+		},
+		entry_prefix = "",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "descending",
+		layout_strategy = "horizontal",
+		file_ignore_patterns = {},
+		border = {},
+		winblend = 0,
+		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+		color_devicons = true,
+		use_less = true,
+		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+		file_sorter = require("telescope.sorters").get_fuzzy_file,
+		generic_sorter = require("telescope.sorters").get_fzf_sorter,
+		extension = require("telescope").load_extension("fzf"),
+	},
+})
