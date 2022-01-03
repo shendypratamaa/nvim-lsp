7e8e36ee45534a8f6b275d1d19d3ca93728db70e
shendy pratama <shendypratamaa@gmail.com>
2022-01-03 20:51:37 +0700

    add incremental selection, and treesitter textobjects

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -34,6 +34,15 @@ treesitter.setup({
 		disable = {},
 		additional_vim_regex_highlighting = false,
 	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "gmm",
+			scope_incremental = "<TAB>",
+			node_decremental = "<S-TAB>",
+		},
+	},
 	rainbow = {
 		enable = true,
 		extended_mode = true,
@@ -57,15 +66,75 @@ treesitter.setup({
 			return disable
 		end, parsers.available_parsers()),
 	},
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
+	textobjects = {
+		select = {
+			enable = true,
+			-- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
+
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["tf"] = "@function.outer",
+				["ti"] = "@function.inner",
+				["tc"] = "@class.outer",
+				["te"] = "@class.inner",
+
+				-- Or you can define your own textobjects like this
+				["iF"] = {
+					python = "(function_definition) @function",
+					cpp = "(function_definition) @function",
+					c = "(function_definition) @function",
+					java = "(method_declaration) @function",
+				},
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					["<TAB>"] = "@parameter.inner",
+				},
+				swap_previous = {
+					["<S-TAB>"] = "@parameter.inner",
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+			},
+			lsp_interop = {
+				enable = true,
+				border = "none",
+				peek_definition_code = {
+					["<C-w>j"] = "@function.outer",
+					["<C-w>k"] = "@class.outer",
+				},
+			},
+		},
+		context_commentstring = {
+			enable = true,
+			enable_autocmd = false,
+		},
+		autopairs = {
+			enable = true,
+		},
+		autotag = {
+			enable = true,
+		},
+		indent = { enable = true, disable = { "yaml", "yml" } },
 	},
-	autopairs = {
-		enable = true,
-	},
-	autotag = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "yaml", "yml" } },
 })
