9b3ff55fbacaacd175fc62668a206b096346444f
shendy pratama <shendypratamaa@gmail.com>
2022-01-06 07:40:13 +0700

    Change Difpanel SetFocus UndoTree, and Change directory cache

--- lua/user/undotree.lua
+++ lua/user/undotree.lua
@@ -3,12 +3,13 @@ vim.g.undotree_ShortIndocators = 1
 vim.g.undotree_HelpLine = 0
 vim.g.undotree_WindowLayout = 3
 vim.g.undotree_SplitWidth = 40
-vim.g.undotree_DiffpanelHeight = 20
-vim.g.undotree_SetFocusWhenToggle = 1
+vim.g.undotree_DiffpanelHeight = 15
+vim.g.undotree_SetFocusWhenToggle = 0
 
 vim.cmd([[
- if has("persistant_undo")
-   set undodir=$HOME/.vim/undodir
+ if has("persistent_undo")
+   set hid
+   set undodir=~/.cache/nvim/undodir
    set undofile
  endif
 ]])
