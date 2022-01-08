4bf80d6ed9f64610a3067ec9b03ce9862ba1c62e
shendy pratama <shendypratamaa@gmail.com>
2022-01-08 12:06:21 +0700

    fix indentline giving colors line for dashboard, and nvimtree

--- lua/user/indentblankline.lua
+++ lua/user/indentblankline.lua
@@ -6,11 +6,29 @@ vim.cmd([[highlight IndentBlanklineIndent5 guifg=#6272a4 gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent6 guifg=#f1fa8c gui=nocombine]])
 
 vim.opt.list = true
+vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_space_char_highlight_list = {
+	"Error",
+	"Function",
+}
+vim.g.indent_blankline_filetype = {
+	"vim",
+	"lua",
+	"html",
+	"markdown",
+	"javascript",
+	"typescript",
+	"javascriptreact",
+	"typescriptreact",
+	"json",
+}
+
+vim.g.indent_blankline_buftype_exclude = {
+	"NvimTree",
+	"Dashboard",
+}
 
 require("indent_blankline").setup({
-	show_current_context = true,
-	show_current_context_start = true,
-	show_end_of_line = true,
 	space_char_blankline = " ",
 	char_highlight_list = {
 		"IndentBlanklineIndent1",
@@ -20,4 +38,5 @@ require("indent_blankline").setup({
 		"IndentBlanklineIndent5",
 		"IndentBlanklineIndent6",
 	},
+	show_trailing_blankline_indent = true,
 })
