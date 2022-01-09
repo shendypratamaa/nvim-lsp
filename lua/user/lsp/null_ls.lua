282a87a63a012e50651ae0a35442a4353621671f
shendy pratama <shendypratamaa@gmail.com>
2022-01-09 10:32:19 +0700

    fix configuration, prettier not working properly, and add eslint for diagnostic document

--- lua/user/lsp/null_ls.lua
+++ lua/user/lsp/null_ls.lua
@@ -11,35 +11,30 @@ local action = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
 
 local sources = {
-	formatting.eslint_d,
-	diagnostics.eslint_d.with({
-		extra_args = { "--config-path", vim.fn.expand("/usr/local/lib/node_modules/eslint_d/bin/") },
-	}),
 	formatting.stylua,
 	formatting.prettier.with({
-		filetype = {
-			"vim",
-			"lua",
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact",
-			"vue",
-			"html",
-			"json",
-			"yaml",
-			"markdown",
+		extra_args = {
+			"--no-semi",
+			"--single-quote",
+			"--jsx-single-quote",
+			"--tsx-single-quote",
 		},
-		extra_args = { "--no-semi", "--single-quotes", "--jsx-single-quotes", "--tsx-single-quotes" },
 	}),
+	diagnostics.eslint,
 	action.gitsigns,
 }
 
 null_ls.setup({
+	debug = false,
 	sources = sources,
 	on_attach = function(client)
 		if client.resolved_capabilities.document_formatting then
-			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+			vim.cmd([[
+          augroup LspFormatting
+              autocmd! * <buffer>
+              autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+          augroup END
+          ]])
 		end
 	end,
 })
