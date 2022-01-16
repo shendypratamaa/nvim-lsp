8c985de59a8ba849b098b372edf8fd12b892e32a
shendy pratama <shendypratamaa@gmail.com>
2022-01-16 19:37:09 +0700

    fix whichkey window popup

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -19,7 +19,7 @@ local setup = {
 			operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
 			motions = false, -- adds help for motions
 			text_objects = false, -- help for text objects triggered after entering an operator
-			windows = true, -- default bindings on <c-w>
+			windows = false, -- default bindings on <c-w>
 			nav = false, -- misc bindings to work with windows
 			z = false, -- bindings for folds, spelling and others prefixed with z
 			g = false, -- bindings for prefixed with g
@@ -54,8 +54,8 @@ local setup = {
 	layout = {
 		height = { min = 3, max = 25 }, -- min and max height of the columns
 		width = { min = 20, max = 50 }, -- min and max width of the columns
-		spacing = 3, -- spacing between columns
-		align = "left", -- align columns left, center or right
+		spacing = 10, -- spacing between columns
+		align = "center", -- align columns left, center or right
 	},
 	ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
 	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
