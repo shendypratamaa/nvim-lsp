371472aa5079a5e0f030325e09025016786da3d3
shendy pratama <shendypratamaa@gmail.com>
2022-01-20 13:37:08 +0700

    FIX

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -25,16 +25,16 @@ npairs.setup({
 	},
 })
 
-if vim.o.ft == "clap_input" and vim.o.ft == "guihua" and vim.o.ft == "guihua_rust" then
-	require("cmp").setup.buffer({ completion = { enable = false } })
-end
-
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
 local cmp_status_ok, cmp = pcall(require, "cmp")
 
 if not cmp_status_ok then
 	return
+end
+
+if vim.o.ft == "clap_input" and vim.o.ft == "guihua" and vim.o.ft == "guihua_rust" then
+	cmp.setup.buffer({ completion = { enable = false } })
 end
 
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
