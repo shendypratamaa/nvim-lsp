51fbf37a8029a4e05c5e9ff6e65e6b78ee2220ca
shendy pratama <shendypratamaa@gmail.com>
2022-01-23 19:21:40 +0700

    fix indentation using stylua formatter

--- init.lua
+++ init.lua
@@ -1,33 +1,33 @@
-require("user.settings")
-require("user.notifications")
-require("user.keymaps")
-require("user.plugins")
-require("user.colorscheme")
-require("user.cmp")
-require("user.lsp")
-require("user.telescope")
-require("user.treesitter")
-require("user.autopairs")
-require("user.autotag")
-require("user.comment")
-require("user.gitsigns")
-require("user.nvimtree")
-require("user.bufferline")
-require("user.lualine")
-require("user.toggleterm")
-require("user.indentblankline")
-require("user.dashboard")
-require("user.colorizer")
-require("user.fold")
-require("user.gps")
-require("user.instant-markdown")
-require("user.lazygit")
-require("user.neorg")
-require("user.neoscroll")
-require("user.numbpeek")
-require("user.scrollbar")
-require("user.trouble")
-require("user.twilight")
-require("user.zenmode")
-require("user.whichkey")
-require("user.undotree")
+require "user.settings"
+require "user.notifications"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.autotag"
+require "user.comment"
+require "user.gitsigns"
+require "user.nvimtree"
+require "user.bufferline"
+require "user.lualine"
+require "user.toggleterm"
+require "user.indentblankline"
+require "user.dashboard"
+require "user.colorizer"
+require "user.fold"
+require "user.gps"
+require "user.instant-markdown"
+require "user.lazygit"
+require "user.neorg"
+require "user.neoscroll"
+require "user.numbpeek"
+require "user.scrollbar"
+require "user.trouble"
+require "user.twilight"
+require "user.zenmode"
+require "user.whichkey"
+require "user.undotree"
