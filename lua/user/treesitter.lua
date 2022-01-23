1ecda1007e3cb432a41a938feda47d79c9a9565d
shendy pratama <shendypratamaa@gmail.com>
2022-01-23 19:26:04 +0700

    fix treesitter issue when running textobj,and textsubj

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,183 +1,172 @@
 local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
 
 if not status_ok then
-	return
+  return
 end
 
-local parsers = require("nvim-treesitter.parsers")
+local parsers = require "nvim-treesitter.parsers"
 
 local enabled_list = {
-	"lua",
-	"javascript",
-	"tsx",
-	"json",
-	"typescript",
-	"javascriptreact",
-	"typescriptreact",
-	"markdown",
+  "lua",
+  "javascript",
+  "tsx",
+  "json",
+  "typescript",
+  "javascriptreact",
+  "typescriptreact",
+  "markdown",
 }
 
 local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
 
 parser_configs.norg = {
-	install_info = {
-		url = "https://github.com/nvim-neorg/tree-sitter-norg",
-		files = { "src/parser.c", "src/scanner.cc" },
-		branch = "main",
-	},
+  install_info = {
+    url = "https://github.com/nvim-neorg/tree-sitter-norg",
+    files = { "src/parser.c", "src/scanner.cc" },
+    branch = "main",
+  },
 }
 
-treesitter.setup({
-	ensure_installed = {
-		"html",
-		"lua",
-		"css",
-		"javascript",
-		"typescript",
-		"vim",
-		"tsx",
-		"json",
-		"cpp",
-		"c",
-		"markdown",
-		"norg",
-	},
-	ignore_install = {},
-	autopairs = {
-		enable = true,
-	},
-	autotag = {
-		enable = true,
-	},
-	highlight = {
-		enable = true,
-		disable = {},
-		additional_vim_regex_highlighting = true,
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "gnn",
-			node_incremental = "gmm",
-			scope_incremental = "gee",
-			node_decremental = "grr",
-		},
-	},
-	refactor = {
-		highlight_definitions = { enable = false },
-		highlight_current_scope = { enable = false },
-		smart_rename = {
-			enable = false,
-			keymaps = {
-				smart_rename = "gaa",
-			},
-		},
-		navigation = {
-			enable = true,
-			keymaps = {
-				goto_definition = "gnd",
-				list_definitions = "gnD",
-				list_definitions_toc = "gO",
-				goto_next_usage = "<Tab>",
-				goto_previous_usage = "<S-Tab>",
-			},
-		},
-	},
-	rainbow = {
-		enable = true,
-		extended_mode = true,
-		colors = {
-			"#ff5874",
-			"#ecc48d",
-			"́#82aaff",
-			"#ae81ff",
-			"#7fdbca",
-			"#a1cd5e",
-			"#b2ceee",
-		},
-		max_file_lines = nil,
-		disable = vim.tbl_filter(function(p)
-			local disable = true
-			for _, lang in pairs(enabled_list) do
-				if p == lang then
-					disable = false
-				end
-			end
-			return disable
-		end, parsers.available_parsers()),
-	},
-	textobjects = {
-		select = {
-			enable = true,
-			-- Automatically jump forward to textobj, similar to targets.vim
-			lookahead = true,
-
-			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
-
-				-- Or you can define your own textobjects like this
-				["iF"] = {
-					python = "(function_definition) @function",
-					cpp = "(function_definition) @function",
-					c = "(function_definition) @function",
-					java = "(method_declaration) @function",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]m"] = "@function.outer",
-					["]]"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]M"] = "@function.outer",
-					["]["] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[m"] = "@function.outer",
-					["[["] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[M"] = "@function.outer",
-					["[]"] = "@class.outer",
-				},
-			},
-			lsp_interop = {
-				enable = true,
-				border = "rounded",
-				peek_definition_code = {
-					["<leader>df"] = "@function.outer",
-					["<leader>dF"] = "@class.outer",
-				},
-			},
-		},
-	},
-	textsubjects = {
-		enable = true,
-		prev_selection = ",",
-		keymaps = {
-			["."] = "textsubjects-smart",
-			[";"] = "textsubjects-container-outer",
-		},
-	},
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-		config = {
-			javascript = {
-				__default = "// %s",
-				jsx_element = "{/* %s */}",
-				jsx_fragment = "{/* %s */}",
-				jsx_attribute = "// %s",
-				comment = "// %s",
-			},
-			typescript = { __default = "// %s", __multiline = "/* %s */" },
-		},
-	},
-	indent = { enable = true, disable = { "yaml" } },
-})
+treesitter.setup {
+  ensure_installed = {
+    "html",
+    "lua",
+    "css",
+    "javascript",
+    "typescript",
+    "vim",
+    "tsx",
+    "json",
+    "cpp",
+    "c",
+    "markdown",
+    "norg",
+  },
+  ignore_install = {},
+  autopairs = {
+    enable = true,
+  },
+  autotag = {
+    enable = true,
+  },
+  highlight = {
+    enable = true,
+    disable = {},
+    additional_vim_regex_highlighting = true,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "gmm",
+      scope_incremental = "gee",
+      node_decremental = "grr",
+    },
+  },
+  refactor = {
+    highlight_definitions = { enable = false },
+    highlight_current_scope = { enable = false },
+    smart_rename = {
+      enable = false,
+      keymaps = {
+        smart_rename = "gaa",
+      },
+    },
+    navigation = {
+      enable = true,
+      keymaps = {
+        goto_definition = "gnd",
+        list_definitions = "gnD",
+        list_definitions_toc = "gO",
+        goto_next_usage = "<Tab>",
+        goto_previous_usage = "<S-Tab>",
+      },
+    },
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    colors = {
+      "#ff5874",
+      "#ecc48d",
+      "́#82aaff",
+      "#ae81ff",
+      "#7fdbca",
+      "#a1cd5e",
+      "#b2ceee",
+    },
+    max_file_lines = nil,
+    disable = vim.tbl_filter(function(p)
+      local disable = true
+      for _, lang in pairs(enabled_list) do
+        if p == lang then
+          disable = false
+        end
+      end
+      return disable
+    end, parsers.available_parsers()),
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          ["]m"] = "@function.outer",
+          ["]]"] = "@class.outer",
+        },
+        goto_next_end = {
+          ["]M"] = "@function.outer",
+          ["]["] = "@class.outer",
+        },
+        goto_previous_start = {
+          ["[m"] = "@function.outer",
+          ["[["] = "@class.outer",
+        },
+        goto_previous_end = {
+          ["[M"] = "@function.outer",
+          ["[]"] = "@class.outer",
+        },
+      },
+      lsp_interop = {
+        enable = true,
+        border = "rounded",
+        peek_definition_code = {
+          ["<leader>df"] = "@function.outer",
+          ["<leader>dF"] = "@class.outer",
+        },
+      },
+    },
+  },
+  textsubjects = {
+    enable = true,
+    prev_selection = ",",
+    keymaps = {
+      ["."] = "textsubjects-smart",
+      [";"] = "textsubjects-container-outer",
+    },
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+    config = {
+      javascript = {
+        __default = "// %s",
+        jsx_element = "{/* %s */}",
+        jsx_fragment = "{/* %s */}",
+        jsx_attribute = "// %s",
+        comment = "// %s",
+      },
+      typescript = { __default = "// %s", __multiline = "/* %s */" },
+    },
+  },
+  indent = { enable = true, disable = { "yaml" } },
+}
