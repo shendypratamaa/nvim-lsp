3b36e9084ede18c21ee15826c3f203d4b139871a
shendy pratama <shendypratamaa@gmail.com>
2022-01-23 19:27:50 +0700

    fix indentation

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,113 +1,121 @@
 local status_ok, lualine = pcall(require, "lualine")
 
-local gps = require("nvim-gps")
+local gps = require "nvim-gps"
 
 local gps_result = {
-	gps.get_location,
-	cond = gps.get_is_available,
+  gps.get_location,
+  cond = gps.get_is_available,
 }
 
 if not status_ok then
-	return
+  return
 end
 
 local hide_in_width = function()
-	return vim.fn.winwidth(0) > 80
+  return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
-	"diagnostics",
-	sources = { "nvim_diagnostic" },
-	sections = { "error", "warn" },
-	symbols = {
-		error = " ",
-		warn = " ",
-	},
-	colored = true,
-	update_in_insert = false,
-	always_visible = true,
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn" },
+  symbols = {
+    error = " ",
+    warn = " ",
+  },
+  colored = true,
+  update_in_insert = false,
+  always_visible = true,
 }
 
 local diff = {
-	"diff",
-	colored = true,
-	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-	cond = hide_in_width,
+  "diff",
+  colored = true,
+  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+  cond = hide_in_width,
 }
 
 local mode = {
-	"mode",
-	fmt = function(str)
-		return "-- " .. str .. " --"
-	end,
+  "mode",
+  fmt = function(str)
+    return "-- " .. str .. " --"
+  end,
 }
 
 local filetype = {
-	"filetype",
-	icons_enabled = true,
-	icon = nil,
+  "filetype",
+  icons_enabled = true,
+  icon = nil,
 }
 
 local branch = {
-	"branch",
-	icons_enabled = true,
-	icon = "🏡",
+  "branch",
+  icons_enabled = true,
+  icon = "🏡",
 }
 
 local location = {
-	"location",
-	padding = 0,
+  "location",
+  padding = 0,
 }
 
 local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
+  local current_line = vim.fn.line "."
+  local total_lines = vim.fn.line "$"
+  local chars = {
+    "__",
+    "▁▁",
+    "▂▂",
+    "▃▃",
+    "▄▄",
+    "▅▅",
+    "▆▆",
+    "▇▇",
+    "██",
+  }
+  local line_ratio = current_line / total_lines
+  local index = math.ceil(line_ratio * #chars)
+  return chars[index]
 end
 
 local filePath = {
-	"filename",
-	icons_enabled = true,
-	icon = "🍰",
-	file_status = true,
-	path = 1,
-
-	symbols = {
-		modified = "[+]", -- Text to show when the file is modified.
-		readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
-		unnamed = "[No Name]", -- Text to show for unnamed buffers.
-	},
+  "filename",
+  icons_enabled = true,
+  icon = "🍰",
+  file_status = true,
+  path = 1,
+  symbols = {
+    modified = "[+]", -- Text to show when the file is modified.
+    readonly = "[-]", -- Text to show when the file is non-modifiable or readonly.
+    unnamed = "[No Name]", -- Text to show for unnamed buffers.
+  },
 }
 
-lualine.setup({
-	options = {
-		icons_enabled = true,
-		theme = "kanagawa", -- available : dracula-nvim, tokyonight, nightfly
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree" },
-		always_divide_middle = true,
-	},
-	sections = {
-		lualine_a = { mode },
-		lualine_b = { branch },
-		lualine_c = { filePath, gps_result },
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diagnostics, diff, "encoding", filetype },
-		lualine_y = { location },
-		lualine_z = { progress },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
-})
+lualine.setup {
+  options = {
+    icons_enabled = true,
+    theme = "auto", -- dracula-nvim, tokyonight, nightfly, kanagawa, gruvbox-material
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = {},
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = { mode },
+    lualine_b = { branch },
+    lualine_c = { filePath, gps_result },
+    lualine_x = { diagnostics, diff, "encoding", filetype },
+    lualine_y = { location },
+    lualine_z = { progress },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  extensions = {},
+}
--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -1,104 +1,105 @@
-vim.g.nvim_tree_icons = {
-	default = "",
-	symlink = "",
-	git = {
-		unstaged = "",
-		staged = "S",
-		unmerged = "",
-		renamed = "➜",
-		deleted = "",
-		untracked = "U",
-		ignored = "◌",
-	},
-	folder = {
-		default = "",
-		open = "",
-		empty = "",
-		empty_open = "",
-		symlink = "",
-	},
-}
-
 local status_ok, nvimtree = pcall(require, "nvim-tree")
-
 local config_ok, nvimtreeconfig = pcall(require, "nvim-tree.config")
-
 if not status_ok and config_ok then
-	return
+  return
 end
 
 local tree_cb = nvimtreeconfig.nvim_tree_callback
 
-nvimtree.setup({
-	disable_netrw = true,
-	hijack_netrw = true,
-	open_on_setup = false,
-	ignore_ft_on_setup = {},
-	update_to_buf_dir = {
-		enable = true,
-		auto_open = true,
-	},
-	auto_close = true,
-	open_on_tab = false,
-	hijack_cursor = false,
-	update_cwd = true,
-	diagnostics = {
-		enable = true,
-		show_on_dirs = true,
-		icons = {
-			hint = "",
-			info = "",
-			warning = "",
-			error = "",
-		},
-	},
-	update_focused_file = {
-		enable = true,
-		update_cwd = true,
-		ignore_list = {},
-	},
-	system_open = {
-		cmd = nil,
-		args = {},
-	},
-	git = {
-		enable = true,
-		ignore = false,
-		timeout = 500,
-	},
-	view = {
-		width = 30,
-		height = 30,
-		side = "left",
-		auto_resize = true,
-		number = false,
-		relativenumber = false,
-		signcolumn = "yes",
-		mappings = {
-			custom_only = false,
-			list = {
-				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-				{ key = "h", cb = tree_cb("close_node") },
-				{ key = "V", cb = tree_cb("vsplit") },
-				{ key = "v", cb = tree_cb("split") },
-			},
-		},
-	},
-	filters = {
-		dotfiles = true,
-		custom = {},
-	},
-	trash = {
-		cmd = "trash",
-		require_confirm = true,
-	},
-	quit_on_open = 0,
-	disable_window_picker = 0,
-	root_folder_modifier = ":t",
-	show_icons = {
-		git = 1,
-		folder = 1,
-		files = 1,
-		folder_arrows = 1,
-	},
-})
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "",
+    staged = "S",
+    unmerged = "",
+    renamed = "➜",
+    deleted = "",
+    untracked = "U",
+    ignored = "◌",
+  },
+  folder = {
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+  },
+}
+
+nvimtree.setup {
+  disable_netrw = true,
+  hijack_netrw = true,
+  open_on_setup = false,
+  auto_close = true,
+  ignore_ft_on_setup = {},
+  update_to_buf_dir = {
+    enable = true,
+    auto_open = true,
+  },
+  open_on_tab = false,
+  hijack_cursor = false,
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {},
+  },
+  system_open = {
+    cmd = nil,
+    args = {},
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 500,
+  },
+  view = {
+    width = 30,
+    height = 30,
+    side = "left",
+    auto_resize = true,
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes",
+    mappings = {
+      custom_only = false,
+      list = {
+        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        { key = "h", cb = tree_cb "close_node" },
+        { key = "V", cb = tree_cb "vsplit" },
+        { key = "v", cb = tree_cb "split" },
+      },
+    },
+  },
+  filters = {
+    dotfiles = true,
+    custom = {},
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+  quit_on_open = 0,
+  disable_window_picker = 0,
+  root_folder_modifier = "%:p:h",
+  show_icons = {
+    git = 1,
+    folder = 1,
+    files = 1,
+    folder_arrows = 1,
+  },
+}
+
+vim.cmd [[
+    hi NvimTreeFolderIcon guifg=#54546D
+]]
