2e428bfea954539323423226bc39fb98d79fff11
shendy pratama <shendypratamaa@gmail.com>
2022-01-23 19:20:55 +0700

    fix indentation

--- lua/user/lsp/null_ls.lua
+++ lua/user/lsp/null_ls.lua
@@ -1,7 +1,6 @@
 local status_ok, null_ls = pcall(require, "null-ls")
-
 if not status_ok then
-	return
+  return
 end
 
 local formatting = null_ls.builtins.formatting
@@ -10,45 +9,45 @@ local actions = null_ls.builtins.code_actions
 -- local diagnostics = null_ls.builtins.diagnostics || for now disable bcs showing twice with navigator
 
 local sources = {
-	formatting.stylua,
-	formatting.prettier.with({
-		filetypes = {
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact",
-			"json",
-			"yaml",
-			"markdown",
-			"css",
-			"html",
-		},
-		extra_args = {
-			"--no-semi",
-			"--single-quote",
-			"--jsx-single-quote",
-			"--tsx-single-quote",
-		},
-	}),
-	formatting.black.with({ extra_args = { "--fast" } }),
-	actions.gitsigns,
-	-- diagnostics.stylelint,
-	-- diagnostics.flake8,
-	-- diagnostics.eslint_d,
+  formatting.stylua,
+  formatting.prettier.with {
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "json",
+      "yaml",
+      "markdown",
+      "css",
+      "html",
+    },
+    extra_args = {
+      "--no-semi",
+      "--single-quote",
+      "--jsx-single-quote",
+      "--tsx-single-quote",
+    },
+  },
+  formatting.black.with { extra_args = { "--fast" } },
+  actions.gitsigns,
+  -- diagnostics.stylelint,
+  -- diagnostics.flake8,
+  -- diagnostics.eslint_d,
 }
 
-null_ls.setup({
-	diagnostics_format = "(#{s}) #{m}",
-	debug = false,
-	sources = sources,
-	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
-			vim.cmd([[
+null_ls.setup {
+  diagnostics_format = "(#{s}) #{m}",
+  debug = false,
+  sources = sources,
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd [[
           augroup LspFormatting
               autocmd! * <buffer>
               autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
           augroup END
-          ]])
-		end
-	end,
-})
+          ]]
+    end
+  end,
+}
