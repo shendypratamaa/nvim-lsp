ba2468a80693aba6fcd38b4606bc0b6ed07bcf4c
shendy pratama <shendypratamaa@gmail.com>
2022-01-25 20:09:17 +0700

    FIX bufferline

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -1,65 +1,74 @@
 local status_ok, buff = pcall(require, "bufferline")
 
 if not status_ok then
-	return
+  return
 end
 
-buff.setup({
-	options = {
-		numbers = nil,
-		close_command = "Bdelete! %d",
-		right_mouse_command = "Bdelete! %d",
-		left_mouse_command = "buffer %d",
-		middle_mouse_command = nil,
-		indicator_icon = "🚀",
-		buffer_close_icon = "",
-		modified_icon = "●",
-		close_icon = "",
-		left_trunc_marker = "",
-		right_trunc_marker = "",
-		name_formatter = function(buf)
-			if buf.name:match("%.md") then
-				return vim.fn.fnamemodify(buf.name, ":t:r")
-			end
-		end,
-		offsets = {
-			{
-				filetype = "NvimTree",
-				text = function()
-					return vim.fn.getcwd()
-				end,
-				highlight = "Directory",
-				text_align = "top",
-				padding = 0,
-			},
-		},
-		max_name_length = 18,
-		max_prefix_length = 15,
-		tab_size = 18,
-		diagnostics = "",
-		diagnostics_update_in_insert = true,
-		-- diagnostics_indicator = function(count, level, diagnostics_dict, context)
-		-- 	return "(" .. count .. ")"
-		-- end,
-		-- custom_filter = fonction(buf_number)
-		-- 	if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-		-- 		return true
-		-- 	end
-		-- 	if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-		-- 		return true
-		-- 	end
-		-- 	if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-		-- 		return true
-		-- 	end
-		-- end,
-		show_buffer_icons = true,
-		show_buffer_close_icons = true,
-		show_close_icon = true,
-		show_tab_indicators = true,
-		persist_buffer_sort = false,
-		separator_style = { " ", " " },
-		enforce_regular_tabs = false,
-		always_show_bufferline = true,
-		sort_by = "relative_directory",
-	},
-})
+buff.setup {
+  options = {
+    numbers = function(opts)
+      return string.format("%s|%s", opts.lower(opts.id), opts.raise(opts.ordinal))
+    end,
+    numbers_style = "subscript",
+    close_command = "Bdelete! %d",
+    right_mouse_command = "Bdelete! %d",
+    left_mouse_command = "buffer %d",
+    middle_mouse_command = nil,
+    indicator_icon = " 🚀",
+    buffer_close_icon = "",
+    modified_icon = "●",
+    close_icon = "",
+    left_trunc_marker = "",
+    right_trunc_marker = "",
+    name_formatter = function(buf)
+      if buf.name:match "%.md" then
+        return vim.fn.fnamemodify(buf.name, ":t:r")
+      end
+    end,
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = function()
+          local cwd = vim.fn.getcwd()
+          return cwd
+        end,
+        highlight = "Directory",
+        text_align = "center",
+        padding = 0,
+      },
+    },
+    max_name_length = 18,
+    max_prefix_length = 15,
+    tab_size = 18,
+    diagnostics = "",
+    diagnostics_update_in_insert = true,
+    -- custom_filter = function(buf_number, buf_numbers)
+    --   -- filter out filetypes you don't want to see
+    --   if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+    --     return true
+    --   end
+    --   -- filter out by buffer name
+    --   if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+    --     return true
+    --   end
+    --   -- filter out based on arbitrary rules
+    --   -- e.g. filter out vim wiki buffer from tabline in your work repo
+    --   if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+    --     return true
+    --   end
+    --   -- filter out by it's index number in list (don't show first buffer)
+    --   if buf_numbers[1] ~= buf_number then
+    --     return true
+    --   end
+    -- end,
+    show_buffer_icons = true,
+    show_buffer_close_icons = true,
+    show_close_icon = true,
+    show_tab_indicators = true,
+    persist_buffer_sort = true,
+    enforce_regular_tabs = true,
+    always_show_bufferline = true,
+    separator_style = { " ", " " },
+    sort_by = "id",
+  },
+}
