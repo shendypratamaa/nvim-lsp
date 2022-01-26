5e6b9dd4d7ee966abdf4f5fe5fdd4b6144fd8b9a
shendy pratama <shendypratamaa@gmail.com>
2022-01-26 13:23:19 +0700

    FIX default indentblankline without colorized

--- lua/user/indentblankline.lua
+++ lua/user/indentblankline.lua
@@ -1,43 +1,44 @@
-vim.cmd([[highlight IndentBlanklineIndent1 guifg=#8be9fd gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent2 guifg=#ff79c6 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent3 guifg=#ffb86c gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent4 guifg=#bd93f9 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent5 guifg=#6272a4 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent6 guifg=#f1fa8c gui=nocombine]])
+-- COLORIZED Options -- default false
+-- vim.cmd([[highlight IndentBlanklineIndent1 guifg=#8be9fd gui=nocombine]])
+-- vim.cmd([[highlight IndentBlanklineIndent2 guifg=#ff79c6 gui=nocombine]])
+-- vim.cmd([[highlight IndentBlanklineIndent3 guifg=#ffb86c gui=nocombine]])
+-- vim.cmd([[highlight IndentBlanklineIndent4 guifg=#bd93f9 gui=nocombine]])
+-- vim.cmd([[highlight IndentBlanklineIndent5 guifg=#6272a4 gui=nocombine]])
+-- vim.cmd([[highlight IndentBlanklineIndent6 guifg=#f1fa8c gui=nocombine]])
 
 vim.opt.list = false
 vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_enabled = false
 vim.g.indent_blankline_space_char_highlight_list = {
-	"Error",
-	"Function",
+  "Error",
+  "Function",
 }
 vim.g.indent_blankline_filetype = {
-	"vim",
-	"lua",
-	"html",
-	"markdown",
-	"javascript",
-	"typescript",
-	"javascriptreact",
-	"typescriptreact",
-	"json",
+  "vim",
+  "lua",
+  "html",
+  "markdown",
+  "javascript",
+  "typescript",
+  "javascriptreact",
+  "typescriptreact",
+  "json",
 }
 
 vim.g.indent_blankline_buftype_exclude = {
-	"NvimTree",
-	"Dashboard",
+  "NvimTree",
+  "Dashboard",
 }
 
-require("indent_blankline").setup({
-	space_char_blankline = " ",
-	char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
-	},
-	show_trailing_blankline_indent = true,
-})
+require("indent_blankline").setup {
+  space_char_blankline = " ",
+  -- char_highlight_list = { -- activate if wanna to be COLORIZED
+  --   "IndentBlanklineIndent1",
+  --   "IndentBlanklineIndent2",
+  --   "IndentBlanklineIndent3",
+  --   "IndentBlanklineIndent4",
+  --   "IndentBlanklineIndent5",
+  --   "IndentBlanklineIndent6",
+  -- },
+  show_trailing_blankline_indent = true,
+}
