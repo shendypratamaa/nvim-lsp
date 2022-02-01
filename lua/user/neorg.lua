f5879935585f41352aca8adba1b34247a916980c
shendy pratama <shendypratamaa@gmail.com>
2022-02-02 00:40:37 +0700

    FIX indentation, add keybinds, change default keybinds to false

--- lua/user/neorg.lua
+++ lua/user/neorg.lua
@@ -1,38 +1,47 @@
 local status_ok, neorg = pcall(require, "neorg")
 
 if not status_ok then
-	return
+  return
 end
 
-neorg.setup({
-	load = {
-		["core.defaults"] = {}, -- Load all the default modules
-		["core.norg.concealer"] = {}, -- Allows for use of icons
-		["core.norg.dirman"] = { -- Manage your directories with Neorg
-			config = {
-				workspaces = {
-					my_workspace = "~/.notes",
-				},
-			},
-		},
-		["core.norg.completion"] = {
-			config = {
-				engine = "nvim-cmp", -- We current support nvim-compe and nvim-cmp only
-			},
-		},
-	},
-})
+neorg.setup {
+  load = {
+    ["core.defaults"] = {}, -- Load all the default modules
+    ["core.norg.concealer"] = {}, -- Allows for use of icons
+    ["core.norg.dirman"] = { -- Manage your directories with Neorg
+      config = {
+        workspaces = {
+          my_workspace = "~/.notes",
+          pNotes = "~/.notes/public/",
+        },
+      },
+    },
+    ["core.norg.completion"] = {
+      config = {
+        engine = "nvim-cmp", -- We current support nvim-compe and nvim-cmp only
+      },
+    },
+    ["core.keybinds"] = {
+      config = {
+        default_keybinds = false,
+        hook = function(keybinds)
+          keybinds.remap_key("norg", "n", "<C-s>", ":w")
+        end,
+      },
+    },
+  },
+}
 
 --- Loads the Neorg completion module
 local function load_completion()
-	neorg.modules.load_module("core.norg.completion", nil, {
-		engine = "nvim-cmp", -- Choose your completion engine here
-	})
+  neorg.modules.load_module("core.norg.completion", nil, {
+    engine = "nvim-cmp", -- Choose your completion engine here
+  })
 end
 
 -- If Neorg is loaded already then don't hesitate and load the completion
 if neorg.is_loaded() then
-	load_completion()
+  load_completion()
 else -- Otherwise wait until Neorg gets started and load the completion module then
-	neorg.callbacks.on_event("core.started", load_completion)
+  neorg.callbacks.on_event("core.started", load_completion)
 end
